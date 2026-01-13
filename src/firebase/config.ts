186b97a9d52cfff07a9992f364356a3fb8ddfcc4
Константин «GyreSuper» Фаткуллин <Gyresuper@gmail.com>
2026-01-13 08:50:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7127577738-5d914",
+  "appId": "1:771064917968:web:b978e8e29d9de95a3153d5",
+  "apiKey": "AIzaSyCkLx7yjaezmJ7YPcqasLWsl4dKnuQUWMk",
+  "authDomain": "studio-7127577738-5d914.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "771064917968"
+};
